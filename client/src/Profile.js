dfbe0b32d9f0eebc06087c3031cbbd8d09e1c507
TaylorDarneille <tmdarneille@dons.usfca.edu>
2018-01-24 15:38:27 -0800

    NOT PUSHING PETER

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state =  {
+      houseName: ''
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({houseName: e.target.value});
+  }
 
   joinHouse = (e) => {
     e.preventDefault();
@@ -9,7 +20,18 @@ class Profile extends Component {
 
   createHouse = (e) => {
     e.preventDefault();
-    console.log("createHouse called!");
+    axios.post('/house/create', {
+      name: this.props.houseName,
+      user: this.props.user
+    });
+  }
+
+  joinHouse = (e) => {
+    e.preventDefault();
+    axios.post('/house/join', {
+      name: this.state.houseName,
+      user: this.props.user
+    });
   }
 
   render(){
@@ -24,15 +46,15 @@ class Profile extends Component {
           <h2>ADD OR JOIN A HOUSE</h2>
           {/*render add/join house form component*/}
 
-          <form className="join-house" onSubmit={this.joinHouse}>
+          <form  className="join-house" onSubmit={this.joinHouse}>
             <label>House Name</label>
-            <input type="text" name="houseName" required/>
+            <input type="text" name="houseName" onChange={this.handleChange} required/>
             <button type="submit">Join House</button>
           </form>
 
           <form className="create-house" onSubmit={this.createHouse}>
             <label>House Name</label>
-            <input type="text" name="houseName" required/>
+            <input type="text" name="houseName" onChange={this.handleChange} required/>
             <button type="submit">Create House</button>
           </form>
 
---
+++ routes/house.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var User = require('../models/user');
+var House = require('../models/house');
+
+router.post('/create', function(req, res, next){
+	var user = req.body.user
+	console.log("house post (create) route called, req.body: "+ req.body);
+	House.create({
+		name: req.body.name
+	}, function(err, house){
+		if(err) {
+			console.log('DB error', err);
+		}
+		else {
+			console.log(user.id);
+			console.log(house);
+			house.users.push(user.id);
+			house.save(function (err) {
+  				if (!err) console.log('Success!');
+			});
+		}
+	})
+});
+
+router.post('/join', function(req, res, next){
+	console.log("house post (join) route called, req.body: "+ req.body);
+});
+
+module.exports = router;
