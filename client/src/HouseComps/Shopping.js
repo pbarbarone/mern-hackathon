9a3ab97bd056c3dbd788ef3939a9b6d66c904a3e
pbarbarone <peter.barbarone@gmail.com>
2018-02-01 12:58:33 -0800

    took shopping snippet from colin.

--- client/src/HouseComps/Shopping.js
+++ client/src/HouseComps/Shopping.js
@@ -8,7 +8,6 @@ class Shopping extends Component {
 			this.state= {
 				newItem: '',
 				date: '',
-				roommateId: '',
 				roommateName:'',
 				dashboard: ''
 			}
@@ -25,7 +24,7 @@ class Shopping extends Component {
 			return(
 				<div className="shopping-container">
 					<h2 className="shopping-header"> Pantry List </h2>
-					<PantryList pantry={this.props.house.shoppingItems} onDelete={this.deleteItem} /> 
+					<PantryList dashboard={this.state.dashboard} user={this.props.user} pantry={this.props.house.shoppingItems} onDelete={this.deleteItem} /> 
 					<ShoppingForm house={this.props.house} refreshList={this.props.refreshList} roommates={this.props.roommates} />
 				</div>
 			)
@@ -33,7 +32,7 @@ class Shopping extends Component {
 			return(
 				<div className="shopping-container">
 					<h2 className="shopping-header"> Pantry List </h2>
-					<PantryList pantry={this.props.house.shoppingItems} onDelete={this.deleteItem} /> 
+					<PantryList dashboard={this.state.dashboard} user={this.props.user} pantry={this.props.house.shoppingItems} onDelete={this.deleteItem} /> 
 				</div>
 				)
 		}else{
@@ -45,14 +44,30 @@ class Shopping extends Component {
 
 class PantryList extends Component {
 	render(){
-			const allItems = this.props.pantry.map(item => {
+		if(this.props.dashboard==="profile"){
+			const userShopp = [];
+			for(var i=0; i < this.props.pantry.length; i++){
+				if(this.props.user.id == this.props.pantry[i].user){
+					userShopp.push(this.props.pantry[i]);
+				}
+			}
+			const userItems = userShopp.map(itemU => {
+				return (<ListItem item={itemU.item} date={itemU.date} roommate={itemU.roommateName} onDelete={this.props.onDelete} />)
+			})
+			return(
+			<ul className ="pantry-list">{userShopp}</ul>
+			)
+		}else if(this.props.dashboard ==="househub"){
+			const allShopp = this.props.pantry.map(item => {
 				return (<ListItem item={item.item} date={item.date} roommate={item.roommateName} onDelete={this.props.onDelete} />)
 			})
-		return(
-			<ul className ="pantry-list">{allItems}</ul>
-
-		)
-	}
+			return(
+			<ul className ="pantry-list">{allShopp}</ul>
+			)
+		}else {
+			console.log("error in shopping conditional");
+		}
+	}	
 }
 
 class ListItem extends Component {
